b0f5e2949623a9a9be536529ff7aed69cff32f41
Brandon Green <brandon@brandonleeproductions.com>
2018-04-10 04:38:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
 
- if (string.toUpperCase() === string) {
+ if (string.toUpperCase("I love you grandma") === string) {
 }
     return "YES INDEED!"
   }
